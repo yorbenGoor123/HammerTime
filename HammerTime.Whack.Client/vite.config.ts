ab80ed06dbf8ee014b73f0349430671b2d9cbcd5
Yorben Goor <gooryor@cronos.be>
2025-02-09 19:36:40 +0100

    add nginx support

--- HammerTime.Whack.Client/vite.config.ts
+++ HammerTime.Whack.Client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {host: true, strictPort: true, port: 8080},
   test: {
     globals: true,
     environment: 'jsdom',
